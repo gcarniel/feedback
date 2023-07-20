f91caecf2a712d2eea3c71392ef4cffa3a4a4196
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-07-20 09:57:53 -0300

    criação tela de login/cadastro

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,8 +1,17 @@
+import { FcFeedback } from "react-icons/fc";
+
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx-auto bg-slate-800 h-screen p-24">
-      <header>header</header>
-      {children}
+    <div className="container mx-auto bg-slate-800 h-screen">
+      <header className="flex items-center justify-between p-4 bg-gray-900 text-white">
+        <div className="flex justify-center w-full">
+          <div className="flex items-center">
+            <FcFeedback className="h-8 w-8 mr-2" />
+            <h1 className="text-lg font-bold">Feedback Flow</h1>
+          </div>
+        </div>
+      </header>
+      <div className="p-24">{children}</div>
     </div>
-  )
+  );
 }
--- src/components/teste.tsx
+++
@@ -1,3 +0,0 @@
-export function ComponenteTeste() {
-  return <h1>Componente teste</h1>
-}

19a37746079ccb3f102fc22c4d852c8168a9a5d3
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-08-11 11:42:18 -0300

    arrumando height das telas

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,7 +3,7 @@ import { FcFeedback } from "react-icons/fc";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx-auto bg-slate-800 h-max">
+    <div className="container mx-auto bg-slate-800 h-fit">
       <header className="flex flex-col items-center justify-center p-4 bg-gray-900 text-white">
         <div className="flex items-center justify-center">
           <FcFeedback className="h-8 w-8 mr-2" />
